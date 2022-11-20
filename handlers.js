8f67650c38e71e662c25ecc8c216e020bb3bfff7
StevanFreeborn <sfreeborn7@gmail.com>
2022-11-20 17:39:25 -0600

    fix: moved references to public folder so i could use them in server and client. fix: account for players first move to not be back on itself. feat: added styling and better status messages about game state

--- handlers.js
+++ handlers.js
@@ -1,9 +1,10 @@
-import keyMappings from './keyMappings.js';
+import keyMappings from './public/scripts/keyMappings.js';
 import { createGameId } from './gameId.js';
 import SocketEvents from './public/scripts/socketEvents.js';
 import { initializeGameState } from './gameState.js';
 import GameModes from './public/scripts/gameModes.js';
 import { createGameInterval } from './gameInterval.js';
+import snakeVelocities from './public/scripts/snakeVelocities.js';
 
 export const handleKeydown = (socket, key, globalState, games) => {
   const game = games[socket.id];
@@ -12,15 +13,47 @@ export const handleKeydown = (socket, key, globalState, games) => {
     return;
   }
 
+  const playerSnake = globalState[game].players[socket.number - 1].snake;
+  const playerSnakeTail = playerSnake[0];
+  const playerSnakeHead = playerSnake[playerSnake.length - 1];
   const currentVelocity = globalState[game].players[socket.number - 1].velocity;
   const newVelocity = keyMappings[key];
 
+  const isFacingRight = (head, tail) => {
+    return head.x > tail.x
+  }
+
+  const isFacingLeft = (head, tail) => {
+    return head.x < tail.x;
+  }
+
   if (newVelocity) {
+    // if snake is moving don't allow it to move in the opposite y direction.
     if (currentVelocity.x != 0 && currentVelocity.x * -1 == newVelocity.x) {
       return;
     } 
 
+    // if snake is moving don't allow it to move in the opposite x direction.
     if (currentVelocity.y != 0 && currentVelocity.y * -1 == newVelocity.y) {
+      return;
+    }
+
+    // if snake is not moving and is facing left and player tries to move snake to the right don't allow it.
+    if (
+      currentVelocity.x == 0 &&
+      isFacingLeft(playerSnakeHead, playerSnakeTail) &&
+      newVelocity.x == snakeVelocities.right.x &&
+      newVelocity.y == snakeVelocities.right.y
+    ) {
+      return;
+    }
+
+    if (
+      currentVelocity.x == 0 &&
+      isFacingRight(playerSnakeHead, playerSnakeTail) &&
+      newVelocity.x == snakeVelocities.left.x &&
+      newVelocity.y == snakeVelocities.left.y
+    ) {
       return;
     }
 
@@ -40,7 +73,7 @@ export const handleNewGame = (io, socket, mode, globalState, games) => {
   socket.emit(SocketEvents.initialize, 1);
 
   if (mode == GameModes.TwoPlayer) {
-    socket.emit(SocketEvents.gameCode, gameId);
+    socket.emit(SocketEvents.gameCode, { message: 'Game code is ', gameCode: gameId });
     return;
   }
 
@@ -64,6 +97,7 @@ export const handleJoinGame = async (io, socket, gameId, globalState, games) => 
   socket.join(gameId);
   socket.number = 2;
   socket.emit(SocketEvents.initialize, 2);
+  socket.emit(SocketEvents.gameCode, { message: 'You\'ve joined game ', gameCode: gameId });
 
   createGameInterval(io, socket, globalState, gameId);
 };
--- public/scripts/game.js
+++ public/scripts/game.js
@@ -1,9 +1,10 @@
 import GameModes from './gameModes.js';
+import keyMappings from './keyMappings.js';
 import SocketEvents from './socketEvents.js';
 
 const BG_COLOR = '#231f20';
-const SNAKE_COLOR_1 = '#c2c2c2';
-const SNAKE_COLOR_2 = '#4651eb'
+const SNAKE_COLOR_1 = '#974fe0';
+const SNAKE_COLOR_2 = '#4651eb';
 const FOOD_COLOR = '#e66916';
 
 const socket = io();
@@ -13,7 +14,9 @@ const mode = params.get('mode');
 const gameId = params.get('gameId');
 
 const gameScreen = document.getElementById('game-screen');
-const gameCodeDisplay = document.getElementById('gameCode');
+const gameCodeMessage = document.getElementById('gameCodeMessage');
+const gameStatus = document.getElementById('gameStatus');
+const playerIdentification = document.getElementById('playerIdentification');
 let canvas;
 let context;
 let playerNumber;
@@ -21,8 +24,8 @@ let isGameActive = false;
 
 const initialize = () => {
   canvas = document.getElementById('canvas');
-  canvas.width = 600;
   canvas.height = 600;
+  canvas.width = 600;
 
   context = canvas.getContext('2d');
   context.fillStyle = BG_COLOR;
@@ -33,6 +36,15 @@ const initialize = () => {
 };
 
 const handleKeydown = e => {
+  const mappedKeys = Object.keys(keyMappings);
+  if (mappedKeys.find(key => key == e.key)) {
+    e.preventDefault();
+  }
+
+  if (isGameActive == false) {
+    return;
+  }
+
   socket.emit(SocketEvents.keyDown, e.key);
 };
 
@@ -50,25 +62,34 @@ const drawGame = state => {
   state.players.forEach((player, index) => {
     const color = index == 0 ? SNAKE_COLOR_1 : SNAKE_COLOR_2;
     drawPlayer(player, size, color);
-  })
+  });
 };
 
 const drawPlayer = (playerState, size, color) => {
   const snake = playerState.snake;
-  context.fillStyle = color;
-
-  snake.forEach(segment =>
-    context.fillRect(segment.x * size, segment.y * size, size, size)
-  );
+  snake.forEach((segment, index, snake) => {
+    const fillColor = index == snake.length - 1 ? 'white' : color;
+    context.fillStyle = fillColor;
+    context.fillRect(segment.x * size, segment.y * size, size, size);
+  });
 };
 
 const handleInitialize = number => {
-  playerNumber = number
+  playerNumber = number;
 };
 
 const handleGameState = gameState => {
   if (isGameActive == false) {
     return;
+  }
+
+  canvas.classList.remove('border-secondary');
+  canvas.classList.add('border-info');
+  gameStatus.innerText = 'Eat the food!';
+
+  if (mode == GameModes.TwoPlayer) {
+    const color = playerNumber == 1 ? SNAKE_COLOR_1 : SNAKE_COLOR_2;
+    playerIdentification.innerHTML = `You are <span class="fw-bold fst-italic" style="color: ${color}">player ${playerNumber}</span>`;
   }
 
   requestAnimationFrame(() => drawGame(gameState));
@@ -79,28 +100,36 @@ const handleGameOver = data => {
     return;
   }
 
-  // TODO: Display a game over message and provide options to quit or play again.
+  canvas.classList.toggle('border-info');
+
   if (data.winner === playerNumber) {
-    alert('You won!');
-    return;
+    gameStatus.innerHTML =
+      '<span class="text-success fw-bold fst-italic">You won!</span> <a class="link-secondary" href="/">Play again?</a>';
+    canvas.classList.toggle('border-success');
+  } else {
+    gameStatus.innerHTML =
+      '<span class="text-danger fw-bold fst-italic">You lost!</span> <a class="link-secondary" href="/">Play again?</a>';
+    canvas.classList.toggle('border-danger');
   }
-  
-  alert('You lost.');
 
   isGameActive = false;
-}
+};
 
-const handleGameCode = gameId => {
-  gameCodeDisplay.innerText = `Your game code is ${gameId}`;
-}
+const handleGameCode = data => {
+  const html = `${data.message}<span id="gameCode" class="fw-bold fst-italic">${data.gameCode}</span>`;
+  gameCodeMessage.innerHTML = html;
+  gameStatus.innerText = 'Waiting for player 2...';
+};
 
 const handleGameNotFound = () => {
-  alert('Game id not found');
-}
+  gameStatus.innerHTML =
+    'Sorry we couldn\'t find that game. Go back to the <a href="/" class="link-success">main menu</a>.';
+};
 
 const handleFullGame = () => {
-  alert('Game is already full');
-}
+  gameStatus.innerHTML =
+    'Sorry this game is already full. Go back to the <a href="/" class="link-success">main menu</a>.';
+};
 
 socket.on(SocketEvents.initialize, handleInitialize);
 socket.on(SocketEvents.gameState, handleGameState);
@@ -112,8 +141,7 @@ socket.on(SocketEvents.gameFull, handleFullGame);
 if (mode == GameModes.TwoPlayer && gameId) {
   socket.emit(SocketEvents.joinGame, gameId);
   initialize();
-} 
-else {
+} else {
   socket.emit(SocketEvents.newGame, mode);
   initialize();
 }
